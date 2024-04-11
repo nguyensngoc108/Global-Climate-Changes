536ac81c8c2616308f9be5c0056743afff64a744
nguyensngoc108 <ititiu20260@student.hcmiu.edu.vn>
2024-04-12 01:05:14 +0700

    Connect mongoDB to server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,22 @@
-console.log("Hello via Bun!");
+import express from "express";
+import dotenv from "dotenv";
+const app = express();
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+import { connectDB } from "./config/db.js";
+import countryRoute from "./routes/countryRoute.js";
+
+import morgan from "morgan";
+
+
+connectDB();
+
+app.use(morgan('combined'));
+app.use(express.json());
+
+app.use("/api/countries", countryRoute);
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
